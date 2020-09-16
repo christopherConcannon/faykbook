f8e0d87868de58ada673bd484ccc931d13c74786
xtophe xanon <xocon10@gmail.com>
2020-09-16 11:26:04 -0500

    start Thought model

---
+++ models/Thought.js
@@ -0,0 +1,28 @@
+const { Schema, model } = require('mongoose');
+const moment = require('moment');
+
+const ThoughtSchema = new Schema(
+	{
+		thoughtText : {
+			type      : String,
+			required  : true,
+			minlength : 1,
+			maxlength : 280
+		},
+		createdAt   : {
+			type    : Date,
+			default : Date.now,
+			get     : (createdAtVal) =>
+				moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+		}
+	},
+	{
+		toJSON : {
+			getters : true
+		}
+	}
+);
+
+const Thought = model('Thought', ThoughtSchema);
+
+module.exports = Thought;
