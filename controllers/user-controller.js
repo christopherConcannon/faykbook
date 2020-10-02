8f1ff335a2241399e85eab7c14038bf7e59663e7
xtophe xanon <xocon10@gmail.com>
2020-10-02 12:18:08 -0500

    update deleteUser controller to delete thoughts

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -66,22 +66,25 @@ const userController = {
 	// 		})
 	// 		.catch((err) => res.status(400).json(err));
 	// },
+  
+  deleteUser({ params }, res) {
+    User.findOneAndDelete({ _id: params.userId })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          return res.status(404).json({ message: 'No user with this id!' });
+        }
 
-	// bonus remove deleted user's thoughts...
-	deleteUser({ params }, res) {
-		Thought.deleteMany({ userId: params.id })
-			.then(() => {
-        User.findOneAndDelete({ _id: params.id })
-          .then((dbUserData) => {
-            if (!dbUserData) {
-              res.status(404).json({ message: 'No user found with this id' });
-              return;
-            }
-					  res.json(dbUserData);
-				});
-			})
-			.catch((err) => res.status(400).json(err));
-	},
+        // get ids of user's `thoughts` and delete them all
+        return Thought.deleteMany({ _id: { $in: dbUserData.thoughts } });
+      })
+      .then(() => {
+        res.json({ message: 'User and associated thoughts deleted!' });
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
 
 	// Friend methods
 	addFriend({ params }, res) {
